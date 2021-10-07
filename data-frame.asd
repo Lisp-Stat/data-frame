66d83e993cc0a1abc2f742d9d858bf5ac878ce7e
Symbolics <steve@symbolics.tech>
2021-10-07 12:16:22 +0800

    Factor variable symbol macros out of independent packages
    The use of packages, whilst keeping with lisp syntax, added an
    additional layer of complexity that was not worth the return on
    investment. The idea will work, but will need some more thought and
    resources put into its implementation, and I don't see any real
    benefit in it at this point.
    
    From the user perspective, all that has changes is that variables are
    referenced 'R style', e.g. mtcars$hp, residing in the LS-USER
    package (or the value of *package* instead of 'lisp style', mtcars:hp,
    with 'hp' residing in the 'mtcars' package.

--- data-frame.asd
+++ data-frame.asd
@@ -1,12 +1,12 @@
 ;;; -*- Mode: LISP; Base: 10; Syntax: ANSI-Common-Lisp; Package: ASDF -*-
 ;;; Copyright (c) 2020-2021 by Symbolics Pte. Ltd. All rights reserved.
 
-(defsystem #:data-frame
+(defsystem "data-frame"
   :version      (:read-file-form "version.sexp")
   :description "Data frames for Common Lisp"
-  :long-description "A data manipulation package, conceptually similar to R's data.frame, but with a lisp-oriented API."
-  :author      "Steve Nunez <steve@symbolics.tech>"
+  :long-description "A data manipulation package, conceptually similar to R's data.frame"
   :author      "Tamas Papp <tkpapp@gmail.com>"
+  :author      "Steve Nunez <steve@symbolics.tech>"
   :licence     :MS-PL
   :source-control (:git "https://github.com/Lisp-Stat/data-frame.git")
   :bug-tracker "https://github.com/Lisp-Stat/data-frame/issues"
@@ -25,21 +25,22 @@
 	       (:file "formatted-output")
 	       (:file "summary")
 	       (:file "defdf")
+	       (:file "properties")
 	       (:file "missing"))
   :in-order-to ((test-op (test-op "data-frame/tests"))))
 
-(defsystem #:data-frame/tests
+(defsystem "data-frame/tests"
   :version "0"
   :description "Unit tests for DATA-FRAME."
-  :author      "Steve Nunez <steve@symbolics.tech>"
   :author      "Tamas Papp <tkpapp@gmail.com>"
+  :author      "Steve Nunez <steve@symbolics.tech>"
   :licence     :MS-PL
   :depends-on ("data-frame"
-               "clunit")		;TODO: upgrade to clunit2. See issue #6
+               "clunit")
   :serial t
   :pathname "tests/"
   :components ((:file "data-frame-tests"))
   :perform (test-op (o s)
 		    (symbol-call :clunit :run-suite
-				      (find-symbol* :data-frame
-							 :data-frame-tests))))
+				 (find-symbol* :data-frame
+					       :data-frame-tests))))
--- src/defdf.lisp
+++ src/defdf.lisp
@@ -12,36 +12,37 @@
 
 (defmacro defdf (df body &optional documentation)
   (when (and documentation (not (stringp documentation))) (error "Data frame documentation is not a string"))
-  `(let* ((df-str (string ',df))
-	  (*package* (if (find-package df-str)    ;exists?
-			 (find-package df-str)    ;yes, return it
-			 (make-package df-str :use '())))) ;no, make it
-     (unless (and *ask-on-redefine*
+     `(unless (and *ask-on-redefine*
 		  (boundp ',df)
 		  (not (y-or-n-p "Data frame has a value. Redefine?")))
-
        (defparameter ,df ,body ,documentation)
        (eval '(setf (name ,df) (symbol-name ',df)))
        (eval '(define-column-names ',df *package*))
        (when ,documentation
 	  (eval '(setf (doc-string ,df) ,documentation)))
        (pushnew ',df *data-frames*)
-       ',df)))
+       ,df))
 
 (defun define-column-names (df package)
   "Create a symbol macro for each column name in DF
 After running this function, you can refer to a column by its name. This is useful if the column names of a data frame have changed.
 Example: (define-column-names mtcars)"
   (maphash #'(lambda (key index)
-	       (let ((col (intern (symbol-name key) package)))
+	       (let ((col (intern (concatenate 'string (symbol-name df) "$" (symbol-name key)) package)))
 		 (export col)
 		 (eval `(cl:define-symbol-macro ,col (aref (columns ,df) ,index)))))
 	   (ordered-keys-table (slot-value (symbol-value df) 'ordered-keys))))
 
-;; TODO
-;; 1. Undefine the symbol macros associated with the data frame
-;; 2. Delete the package associated with the data frame
-;; See https://stackoverflow.com/questions/69296485/undefining-a-symbol-macro
+(defun undef-column-names (df)
+  "Remove symbol macro for each column name in DF"
+  (maphash #'(lambda (key index)
+	       (declare (ignore index))
+	       (let ((sym (concatenate 'string (symbol-name df) "$" (symbol-name key))))
+		 ;; (format t "Uninterning ~A " sym)
+		 (unintern (find-symbol sym))))
+	   (ordered-keys-table (slot-value (symbol-value df) 'ordered-keys))))
+
+;;; TODO make this work on *variables* and *data-frames*
 (defun undef (df)
   "If DF is the symbol of a defined data-frame it is unbound and
 removed from the list of data-frames. If DF is a list of data-frame
@@ -50,28 +51,48 @@ names each is unbound and removed. Returns DF.
 Example: (undef 'mtcars)"
   (dolist (s (if (listp df) df (list df)))
     (when (member s *data-frames*)
+      (undef-column-names df)
       (setf *data-frames* (delete s *data-frames*))
       (makunbound s)))
   df)
 
-(defun show-data-frames (&optional (stream *standard-output*))
-  "Print all data frames in the current environment in reverse order of creation, i.e. most recently created first."
-  (loop for df in *data-frames* do
-  ;; "Print all data frames in the current environment in alphabetical order"
-  ;; (loop for df in (sort (copy-list *data-frames*) #'string<=) do
-    (print-object (symbol-value df) stream)
-    (fresh-line stream)
-    (terpri stream)))
+;;; In order to show data frame consistently with different settings
+;;; for print-object, we need to control printing here.
+(defun show-data-frames (&key (head nil) (stream *standard-output*))
+  "Print all data frames in the current environment in reverse order of creation, i.e. most recently created first.
+if HEAD is not NIL, print the first six rows, similar to the (head) function"
+    (loop for df-sym in *data-frames* do
+      ;; "Print all data frames in the current environment in alphabetical order"
+      ;; (loop for df in (sort (copy-list *data-frames*) #'string<=) do
+      (let ((df (symbol-value df-sym)))
+	(if head
+	    (let* ((*print-lines* 6)
+		   (*print-pretty* t))
+	      (when (slot-boundp df 'name) (format stream "~A" (name df)))
+	      (df:pprint-data-frame df stream nil))
 
+	  ;; Not head
+	    (print-unreadable-object (df stream :type t)
+	      (when (slot-boundp df 'name) (format stream "~A " (name df)))
+	      (format stream "(~d observations of ~d variables)"
+		      (aops:nrow df)
+		      (aops:ncol df))
+	      (when (slot-boundp df 'doc-string)
+		(fresh-line stream)
+		(format stream "~A" (doc-string df))))))
+      (fresh-line stream)
+      (terpri stream)))
+
+;;; TODO Make this a function: (replace-key! (df 'new 'old) ...
 (defmacro replace-key! (df new old)
   "Replace a key in DF, updating data package symbols
 Example: (replace-key! mtcars row-name x1)"
-  `(let* ((*package* (find-package (string-upcase (string ',df))))
-	  (sym (intern (string ',new)))
-	  (old-key (find-symbol (string ',old))))
+  `(let ((sym (intern (string ',new)))
+	 (old-key (find-symbol (string ',old))))
      (export sym)
      (substitute-key! ,df sym old-key)
      (unintern old-key)
+     (unintern (find-symbol (concatenate 'string (symbol-name ',df) "$" (symbol-name ',old))))
      (funcall #'define-column-names ',df *package*)))
 
 ;; Unexported. For debugging
